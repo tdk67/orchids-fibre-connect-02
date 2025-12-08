b62767dfc238e65c06c20f104c45b99a123a63f6
kaffenberger <kaffenberger@1und1-versatel-business-partner.net>
2025-12-08 11:27:12 +0000

    File changes

--- src/pages/Kalender.jsx
+++ src/pages/Kalender.jsx
@@ -30,32 +30,19 @@ export default function Kalender() {
       </div>
 
       {/* Calendar Content */}
-      {calendarLink ? (
-        <Card className="border-0 shadow-md">
-          <CardContent className="p-0">
-            <div className="w-full" style={{ height: 'calc(100vh - 200px)' }}>
-              <iframe
-                src={calendarLink}
-                className="w-full h-full rounded-lg"
-                frameBorder="0"
-                scrolling="no"
-              />
-            </div>
-          </CardContent>
-        </Card>
-      ) : (
-        <Card className="border-0 shadow-md">
-          <CardContent className="p-12">
-            <Alert>
-              <AlertCircle className="h-4 w-4" />
-              <AlertDescription>
-                Kein Google Kalender-Link hinterlegt. Bitte kontaktieren Sie Ihren Administrator, 
-                um einen Kalender-Link in Ihren Mitarbeiter-Einstellungen zu hinterlegen.
-              </AlertDescription>
-            </Alert>
-          </CardContent>
-        </Card>
-      )}
+      <Card className="border-0 shadow-md">
+        <CardContent className="p-0">
+          <div className="w-full" style={{ height: 'calc(100vh - 200px)' }}>
+            <iframe
+              src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=Europe%2FBerlin&showPrint=0&src=a2FmZmkxOTg4QGdtYWlsLmNvbQ&src=NWE3MDUwZWFjZjE3NzFhODMwNzA5MWJlNmNlZGVlY2Y0NWU2YjNhOWU4ODZiMjI4YmNjOWU1MzAxZjE0YzY2OEBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NDM0YzE1NjE0NTU4ZWE2ZWIwOWExMzNkYzcyMDdlMTJmYzExNTQ0YzQ2ZDhlZTU2YzU3YmFkZDg0MzQ2ODBjYkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=OTg5NTU2MjhjYjg1N2FhOTM2OGIwNWRlY2M4MmEwMGNmN2VhOGU2M2Y4NTI4NjE2Nzc5MTQ5Y2Q2OWUzNmQwZUBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZDc1Y2ZmZWRjMGE4NzhmN2VhMzY1ODI3MDc0Mjc1YTY0ZWI1NjUzMjNiODAzNjgwYmQyM2IwZDhjYWQ1YTJmY0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=NGE5ZWZlN2NmNTI2NjZkYjU2NDYyYmQzOGI1OGZiNDY1ZTFhMmRlNjEzMDYwNTYzYjIwNzY1MDcwMWFjNzc2YkBncm91cC5jYWxlbmRhci5nb29nbGUuY29t&src=ZGUuZ2VybWFuI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZGFuY2FrbGVvbmFyZG9AZ21haWwuY29t&color=%23039be5&color=%23a79b8e&color=%23d81b60&color=%23795548&color=%23b39ddb&color=%23f09300&color=%230b8043&color=%239e69af"
+              className="w-full h-full rounded-lg border border-slate-200"
+              style={{ border: 'solid 1px #777' }}
+              frameBorder="0"
+              scrolling="no"
+            />
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
 }
