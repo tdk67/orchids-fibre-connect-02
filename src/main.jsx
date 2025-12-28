ab70720b917a5fd3e36b9e9a67ee4f79d0b366b7
tdk67 <tdeak67@googlemail.com>
2025-12-29 00:37:16 +0100

    Auto-sync from Orchids Desktop

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import { AuthProvider } from '@/lib/AuthContext'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClientInstance } from '@/lib/query-client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <App />
+  <AuthProvider>
+    <QueryClientProvider client={queryClientInstance}>
+      <App />
+    </QueryClientProvider>
+  </AuthProvider>
   // </React.StrictMode>,
 )
 
