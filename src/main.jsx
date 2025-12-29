1c802801c8d4e98cceab9989c59380b5b4dec029
tdk67 <tdeak67@googlemail.com>
2025-12-30 00:19:58 +0100

    Auto-sync from Orchids Desktop

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import { AuthProvider } from '@/lib/AuthContext'
+import { AuthProvider } from '@/lib/AuthContext.jsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 
